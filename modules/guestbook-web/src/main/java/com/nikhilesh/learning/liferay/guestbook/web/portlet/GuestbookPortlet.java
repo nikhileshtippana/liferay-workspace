f51446c323d26bf11cc2d502d8fb3946945a11ec
nikhileshtippana <nikhilesh.nike@gmail.com>
2017-12-07 22:30:48 -0500

    Added jsp files to portal-settings-web. Added 'expiration-cache' init parameter to guestbook portlet

--- modules/guestbook-web/src/main/java/com/nikhilesh/learning/liferay/guestbook/web/portlet/GuestbookPortlet.java
+++ modules/guestbook-web/src/main/java/com/nikhilesh/learning/liferay/guestbook/web/portlet/GuestbookPortlet.java
@@ -16,6 +16,9 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
@@ -36,11 +39,11 @@ import com.nikhilesh.learning.liferay.guestbook.web.constants.GuestbookPortletKe
 @Component(
 		immediate = true,
 		property = {
-			"com.liferay.portlet.display-category=category.social",
+			"com.liferay.portlet.display-category=category.nikapps",
 			"com.liferay.portlet.instanceable=false",
 			"com.liferay.portlet.scopeable=true",
 			"javax.portlet.display-name=Guestbook",
-			"javax.portlet.expiration-cache=0",
+			"javax.portlet.expiration-cache=60",
 			"javax.portlet.init-param.template-path=/",
 			"javax.portlet.init-param.view-template=/guestbookwebportlet/view.jsp",
 			"javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
@@ -79,6 +82,10 @@ public class GuestbookPortlet extends MVCPortlet {
 			}
 
 			renderRequest.setAttribute("guestbookId", guestbookId);
+			
+			User user = PortalUtil.getUser(renderRequest);
+			
+			_log.info("Rendered by " + user.getFullName());
 		}
 		catch (Exception e) {
 			throw new PortletException(e);
@@ -174,5 +181,7 @@ public class GuestbookPortlet extends MVCPortlet {
 
 	protected EntryLocalService _entryLocalService;
 	protected GuestbookLocalService _guestbookLocalService;
+	
+	static final Log _log = LogFactoryUtil.getLog(GuestbookPortlet.class);
 
 }
